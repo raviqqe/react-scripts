6784f9c660918a992c4cd14aeef6c2c54901b2f6
Yota Toyama <raviqqe@gmail.com>
2020-11-21 15:13:24 -0800

    Fix build

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@ import { runTests } from "./test";
 
 const [command, ...args] = process.argv.slice(2);
 
-switch (command) {
-  case "build":
-    await build();
-    break;
-  case "test":
-    await runTests(args);
-    break;
-  default:
-    console.error("Usage: react-scripts {build,lint,test}");
-    process.exit(1);
-}
+(async (): Promise<void> => {
+  switch (command) {
+    case "build":
+      await build();
+      break;
+    case "test":
+      await runTests(args);
+      break;
+    default:
+      console.error("Usage: react-scripts {build,lint,test}");
+      process.exit(1);
+  }
+})().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
