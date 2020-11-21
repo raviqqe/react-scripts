62a5b9db1f03356de2a646d7d1b20c0789572d3e
Yota Toyama <raviqqe@gmail.com>
2020-11-21 14:11:28 -0800

    Connect test command

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import { build } from "./build";
+import { test } from "./test";
 
-const subcommand = process.argv[2];
+const [command, ...args] = process.argv.slice(2);
 
-switch (subcommand) {
+switch (command) {
   case "build":
     await build();
+    break;
+  case "test":
+    await test(args);
     break;
   default:
     console.error("Usage: react-scripts {build,lint,test}");
