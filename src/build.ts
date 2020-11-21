4444b68f1bb00286c1a442186c35b6f6a090b6fc
Yota Toyama <raviqqe@gmail.com>
2020-11-21 13:55:09 -0800

    Add build module

---
+++ src/build.ts
@@ -0,0 +1,32 @@
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
+import { rollup } from "rollup";
+import { terser } from "rollup-plugin-terser";
+import { generateSW } from "rollup-plugin-workbox";
+import { compilerOptions } from "./compiler-options";
+
+export const build = async (): Promise<void> =>
+  rollup({
+    input: "src/index.ts",
+    output: {
+      dir: "public",
+      format: "es",
+    },
+    plugins: [
+      commonjs(),
+      json(),
+      generateSW({
+        globDirectory: "public",
+        swDest: "public/service-worker.js",
+      }),
+      nodeResolve(),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      }),
+      terser(),
+      typescript(compilerOptions),
+    ],
+  });
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,13 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
-import typescript from "@rollup/plugin-typescript";
-import { rollup } from "rollup";
-import { terser } from "rollup-plugin-terser";
-import { generateSW } from "rollup-plugin-workbox";
-import { compilerOptions } from "./compiler-options";
+import { build } from "./build";
 
 const subcommand = process.argv[2];
 
 switch (subcommand) {
   case "build":
-    await rollup({
-      input: "src/index.ts",
-      output: {
-        dir: "public",
-        format: "es",
-      },
-      plugins: [
-        commonjs(),
-        json(),
-        generateSW({
-          globDirectory: "public",
-          swDest: "public/service-worker.js",
-        }),
-        nodeResolve(),
-        replace({
-          "process.env.NODE_ENV": JSON.stringify("production"),
-        }),
-        terser(),
-        typescript(compilerOptions),
-      ],
-    });
+    await build();
     break;
   default:
     console.error("Usage: react-scripts {build,lint,test}");
