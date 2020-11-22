dc1472242cbbb0ee8d18a62e11e384649eaaaee9
Yota Toyama <raviqqe@gmail.com>
2020-11-22 01:19:03 -0800

    Dependabot configuration (#10)
    * Refactor
    
    * Add dependabot.yml

--- src/init.ts
+++ src/init.ts
@@ -1,10 +1,10 @@
 import { symlink } from "fs/promises";
-import { relative, resolve } from "path";
+import { relative, join } from "path";
 
 const tsConfigPath = "tsconfig.json";
 
 export const init = async (): Promise<void> => {
   // TODO Remove this link when @typescript-eslint/parser allows passing
   // compiler options directly in JavaScript.
-  await symlink(relative(".", resolve(__dirname, tsConfigPath)), tsConfigPath);
+  await symlink(relative(".", join(__dirname, tsConfigPath)), tsConfigPath);
 };
