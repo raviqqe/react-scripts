c964ed576c05da98649fb1f649512bc4ee5cd39a
Yota Toyama <raviqqe@gmail.com>
2020-11-21 16:12:03 -0800

    Fix build script

--- src/build.ts
+++ src/build.ts
@@ -3,31 +3,35 @@ import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import typescript from "@rollup/plugin-typescript";
-import { rollup } from "rollup";
+import { rollup, RollupOptions } from "rollup";
 import { terser } from "rollup-plugin-terser";
 import { generateSW } from "rollup-plugin-workbox";
 import { compilerOptions } from "./compiler-options";
 
+const options: RollupOptions = {
+  input: "src/index.ts",
+  output: {
+    dir: "public",
+    format: "es",
+  },
+  plugins: [
+    commonjs(),
+    json(),
+    generateSW({
+      globDirectory: "public",
+      swDest: "public/service-worker.js",
+    }),
+    nodeResolve(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+    terser(),
+    typescript(compilerOptions),
+  ],
+};
+
 export const build = async (): Promise<void> => {
-  await rollup({
-    input: "src/index.ts",
-    output: {
-      dir: "public",
-      format: "es",
-    },
-    plugins: [
-      commonjs(),
-      json(),
-      generateSW({
-        globDirectory: "public",
-        swDest: "public/service-worker.js",
-      }),
-      nodeResolve(),
-      replace({
-        "process.env.NODE_ENV": JSON.stringify("production"),
-      }),
-      terser(),
-      typescript(compilerOptions),
-    ],
-  });
+  const build = await rollup(options);
+
+  await build.write(options.output);
 };
