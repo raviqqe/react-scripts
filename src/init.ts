7247c0c09a6de4d6c21197a9a6ebf38d56b30f4c
Yota Toyama <raviqqe@gmail.com>
2020-11-22 08:11:07 +0000

    Add comment

--- src/init.ts
+++ src/init.ts
@@ -4,5 +4,7 @@ import { resolve } from "path";
 const tsConfigPath = "tsconfig.json";
 
 export const init = async () => {
+  // TODO Remove this link when @typescript-eslint/parser allows passing
+  // compiler options directly in JavaScript.
   await copyFile(resolve(__dirname, tsConfigPath), tsConfigPath);
 };
